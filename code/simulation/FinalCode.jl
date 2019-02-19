2ed594d194553ce6b8adb6ceb75912347c5954fa
ppxasjsm <antonia.mey@fu-berlin.de>
2019-02-19 11:12:17 +0000

    README.md

--- code/simulation/FinalCode.jl
+++ code/simulation/FinalCode.jl
@@ -1,12 +1,14 @@
 
-# first run (strg+enter) the cell at the very bottom of this notebook to define all functions
+# 1. Create a folder "results" in the directory where this code is run. The output files are written there.
+# 2. Run (ctrl+enter) the cell at the very bottom of this notebook to load packages and define all functions required.
+# 3. Run "parameter specification".
+# 4. Run "simulation".
 
-# the output files are
+# The output files are
 # a) the coordinates of the final configuration (x1,y1,z1,x2,y2,z2,x3,...)
 # b) the final configuration's radius of gyration
 # c) the final adjacency matrix. '1' means linked, '2' means not linked
 
-#here we define all parameters
 #default values correspond to the chain of equally-sized spheres described in the main text
 
 N=10              #number of spheres
@@ -15,93 +17,96 @@ K=50              #spring constant for harmonic potential. The higher, the more 
 dt=0.8*1e-2       #numerical time step 
 ΔT=0.15           #time interval after which shrinking the distance between chosen spheres is checked
 A=5               #"bending stiffness" of initial chain. The larger, the straighter.
-NumberOfSims=30; #how many independent simulations are run for this parameter set
+NumberOfSims=1;   #how many independent simulations are run for this parameter set
 
-#start simulations. output files are written at the bottom of this cell.
 for sim in 1:NumberOfSims
 
-tic()
-
-r=0.5*(ones(N)+σ*(rand(N)-0.5)*2)  #sphere radii for THIS simulation
-d=r[1:N-1]+r[2:N]                  #corresponding bond lengths
-R0=SetIC(N,A,d);                   #draw initial condition from Boltzmann distribution
+    r=0.5*(ones(N)+σ*(rand(N).-0.5)*2)  #sphere radii for THIS simulation
+    d=r[1:N-1]+r[2:N]                  #corresponding bond lengths
+    R0=SetIC(N,A,d);                   #draw initial condition from Boltzmann distribution
     
-M=zeros(N,N)                       #generate adjacency matrix
-M[1,1]=M[1,2]=M[N,N]=M[N,N-1]=2    #set (next) diagonal to 1
-for i=2:N-1
-  M[i,i-1]=M[i,i]=M[i,i+1]=2
-end
-
-
-#First Bond
-pair=FindPair(M)                   #Choose untried entry (=0) in M
-p=pair[1];q=pair[2]
-
-                                   #first one manually for programming syntax reasons ('bonds')
-R0=Simulate(R0,dt,ΔT,d,p,q,K,[],r) #run simulations in which p and q are pulled together
-M[p,q]=M[q,p]=1                    #assume first one worked.
-bonds=[p q]                        #keeps track of the successfully formed bonds
-
-
-for i in 2:(N^2-N-2*(N-1))/2       #loop over number of remaining zeros in M
-    pair=FindPair(M)               #Choose untried entry (=0) in M
-    p=pair[1];q=pair[2]
-    Rstart=R0;                     #remember state before this cycle
-    R0=Simulate(R0,dt,ΔT,d,p,q,K,bonds,r)                      #run simulations in which p and q are pulled together
-    if norm(R0[(3p-2):(3p)]-R0[(3q-2):(3q)]) < r[p]+r[q]       #check distance between p and q after simulation.
-        M[p,q]=M[q,p]=1            #in case of success matrix entry to '1' and add new bond
-        bonds=[bonds; [p q]]
-    else
-        M[p,q]=M[q,p]=2            #in case of no success set matrix entry to '2' and restore configuration before this cycle
-        R0=Rstart;
+    M=zeros(N,N)                       #generate adjacency matrix
+    M[1,1]=M[1,2]=M[N,N]=M[N,N-1]=1    #set (next) diagonal to 1
+    for i=2:N-1
+        M[i,i-1]=M[i,i]=M[i,i+1]=1
     end
+
+
+    #First Bond manually for programming syntax reasons ('bonds')
+    pair=FindPair(M)                   #Choose untried entry (=0) in M
+    p=pair[1];q=pair[2]
+    R0=Simulate(R0,dt,ΔT,d,p,q,K,[],r) #run simulations in which p and q are pulled together
+    M[p,q]=M[q,p]=1                    #assume first one worked.
+    bonds=[p q]                        #keeps track of the successfully formed bonds
+
+
+    for i in 2:(N^2-N-2*(N-1))/2       #loop over number of remaining zeros in M
+        pair=FindPair(M)               #Choose untried entry (=0) in M
+        p=pair[1];q=pair[2]
+        Rstart=R0;                     #remember state before this cycle
+        R0=Simulate(R0,dt,ΔT,d,p,q,K,bonds,r)                      #run simulations in which p and q are pulled together
+        if norm(R0[(3p-2):(3p)]-R0[(3q-2):(3q)]) < r[p]+r[q]       #check distance between p and q after simulation.
+            M[p,q]=M[q,p]=1            #in case of success matrix entry to '1' and add new bond
+            bonds=[bonds; [p q]]
+        else
+            M[p,q]=M[q,p]=2            #in case of no success set matrix entry to '2' and restore configuration before this cycle
+            R0=Rstart;
+        end
 
         
 
-    if mod(i,100)==0               #every 100th cycle, we check if we can already exclude some untried bonds
+        if mod(i,100)==0               #every 100th cycle, we check if we can already exclude some untried bonds
             
-        # check whether there are more than 8N ones in M, as this would mean we've achieved the 'closest ball packing' possible in 3D
-        MatCount=0
-        for element in M
-            if element==1
-               MatCount+=1
-            end
-        end
-        if MatCount>(8*N)-1
+            # check whether there are more than 8N ones in M, as this would mean we've achieved the 'closest ball packing' possible in 3D
+            MatCount=0
             for element in M
-                if element==0
-                   element=2
-                end
-            end
-        end
-
-        # check whether any bead is bonded to 12 other beads, as this is the maximum number of same-sized spheres a sphere can touch.
-        for row in 1:N
-            LineCount=0
-            for element in M[row,:]
                 if element==1
-                    LineCount+=1
+                    MatCount+=1
                 end
             end
-            if LineCount>11
-                for element in M[row,:]
+            if MatCount>(8*N)-1
+                for element in M
                     if element==0
                         element=2
                     end
                 end
             end
+
+            # check whether any bead is bonded to 12 other beads, as this is the maximum number of same-sized spheres a sphere can touch.
+            for row in 1:N
+                LineCount=0
+                for element in M[row,:]
+                    if element==1
+                        LineCount+=1
+                    end
+                end
+                if LineCount>11
+                    for element in M[row,:]
+                        if element==0
+                            element=2
+                        end
+                    end
+                end
+            end
         end
     end
-end
 
-#write results to files
-writedlm("results/AdjacencyMatrix_N$(N)_$(sim).txt",M)
-writedlm("results/FinalCoordinates_N$(N)_$(sim).txt",R0)
-writedlm("results/RadiusOfGyration_N$(N)_$(sim).txt",Rgyration(R0,1,N))
-
-toc()
+    #write results to files
+    writedlm("results/AdjacencyMatrix_N$(N)_$(sim).txt",M)
+    writedlm("results/FinalCoordinates_N$(N)_$(sim).txt",R0)
+    writedlm("results/RadiusOfGyration_N$(N)_$(sim).txt",Rgyration(R0,1,N))
     
 end
+
+#PACKAGES
+
+using LinearAlgebra
+using Statistics
+using DelimitedFiles
+
+
+
+#FUNCTIONS
 
 # total force vector
 function f(R,d,p,q,K,bonds,NB,r)
@@ -190,7 +195,7 @@ for i in 1:N
           Force[(3i-2):(3i)]+=WW
           Force[(3j-2):(3j)]-=WW
         end
-        deleteat!(NB[j],findin(NB[j],i))
+        deleteat!(NB[j],findall((in)(i), NB[j]))
       end
     end 
 end
@@ -225,8 +230,7 @@ end
 function NBList(R,threshold)
 
 N=Integer(length(R)/3)
-
-NB = Array{Any}(N)
+NB = Array{Any}(undef,N)
 for index in eachindex(NB)
   NB[index]=Int64[]
 end
@@ -367,9 +371,9 @@ end
     
 end
 
-R[1:3:end-2]-=mean(R[1:3:end-2])
-R[2:3:end-1]-=mean(R[2:3:end-1])
-R[3:3:end]-=mean(R[3:3:end]);
+R[1:3:end-2].-=mean(R[1:3:end-2])
+R[2:3:end-1].-=mean(R[2:3:end-1])
+R[3:3:end].-=mean(R[3:3:end]);
     
 return R
 
